93d2d59cc1db08cd3379458b35966498c4262e43
Marco Ortega <MarcoOrt11@gmail.com>
2023-12-15 00:59:39 -0500

    Python-syntax

---
+++ projects/python-syntax/words.py
@@ -0,0 +1,6 @@
+def print_upper_words3(words, must_start_with):
+    for word in words:
+        for letter in must_start_with:
+            if word.startswith(letter):
+                print(word.upper())
+                break
